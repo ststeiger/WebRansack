c121bc4defba1cd4b141dd202b988c585d71ace9
ststeiger <quandary82@hailmail.net>
2018-10-05 10:01:56 +0200

    memory optimization

--- WebRansack/Program.cs
+++ WebRansack/Program.cs
@@ -16,10 +16,69 @@ namespace WebRansack
     
     public class Program
     {
-        
-        
+
+        // https://www.heroku.com/free
+        // https://medium.com/@AndreyAzimov/how-free-heroku-really-works-and-how-to-get-maximum-from-it-daa53f2b3c57
+
+        public static void GetProcesses()
+        {
+            // https://cloud.google.com/appengine/docs/flexible/dotnet/quickstart
+            // https://codelabs.developers.google.com/codelabs/cloud-app-engine-aspnetcore/#0
+            // https://www.hanselman.com/blog/TryingASPNETCoreOnTheGoogleCloudPlatformAppEngineFlexibleEnvironment.aspx
+            // https://cloud.google.com/free/
+
+            System.Diagnostics.Process[] pls = System.Diagnostics.Process.GetProcesses();
+        }
+
+
+        // https://blog.discountasp.net/reducing-net-core-memory-usage/
+        // <PropertyGroup>
+        //    <TargetFramework>netcoreapp2.0</TargetFramework>
+        //    <TypeScriptToolsVersion>2.5</TypeScriptToolsVersion>
+        //    <ServerGarbageCollection>false</ServerGarbageCollection>
+        // </PropertyGroup>
+        public static async Task GetProcess(System.Diagnostics.Process proc)
+        {
+            // https://stackoverflow.com/questions/47656988/viewing-memory-usage-stats-of-a-dotnetcore-2-self-contained-application-on-linux
+            long currentMemoryUsage = proc.WorkingSet64;
+            long peakPhysicalMemoryUsage = proc.PeakWorkingSet64;
+
+            currentMemoryUsage /= 1048576; // 1048576 = 1024^2 = MB
+            peakPhysicalMemoryUsage /= 1048576; // 1048576 = 1024^2 = MB
+            
+            System.Console.WriteLine("Current: " + currentMemoryUsage.ToString(System.Globalization.CultureInfo.InvariantCulture) + "MB");
+            System.Console.WriteLine("Peak: " + peakPhysicalMemoryUsage.ToString(System.Globalization.CultureInfo.InvariantCulture) + "MB");
+            
+            await Task.CompletedTask;
+        }
+
+
+        public static async Task PeriodicTask(TimeSpan interval, System.Threading.CancellationToken cancellationToken)
+        {
+            using (System.Diagnostics.Process proc = System.Diagnostics.Process.GetCurrentProcess())
+            {
+
+                while (true)
+                {
+                    await GetProcess(proc);
+                    await Task.Delay(interval, cancellationToken);
+                }
+
+            }
+        }
+
+
+        public static async Task PeriodicTask(int interval, System.Threading.CancellationToken cancellationToken)
+        {
+            System.TimeSpan ts = new System.TimeSpan(0, 0, interval);
+            await PeriodicTask(ts, cancellationToken);
+        }
+
+
+
         public static void Main(string[] args)
         {
+            PeriodicTask(10, System.Threading.CancellationToken.None);
             BuildWebHost(args).Run();
         }
         

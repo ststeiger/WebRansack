570692679bd2a21d5c08a05237cd41d1f62510e5
ststeiger <quandary82@hailmail.net>
2019-10-09 16:58:53 +0200

    AhoCorasick

---
+++ WebRansack/Code/SearchAlgorithms/BoyerMoore.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WebRansack.Code.SearchAlgorithms
+{
+
+
+    // https://github.com/harvey007y/FindTextInFiles
+    // https://stackoverflow.com/questions/37979457/fastest-way-to-search-string-in-large-text-file
+    // https://www.codeproject.com/Articles/34451/Multithreaded-File-Folder-Finder
+    // https://stackoverflow.com/questions/48543469/file-search-optimisation-in-c-sharp-using-parallel
+    // https://www.thomashutter.com/facebook-der-unterschied-zwischen-edgerank-und-graphrank/
+    // https://stackoverflow.com/questions/48543469/file-search-optimisation-in-c-sharp-using-parallel
+    // https://codereview.stackexchange.com/questions/124038/performing-parallel-processing-on-a-file
+
+    
+    // https://www.wipfli.com/insights/blogs/connect-microsoft-dynamics-365-blog/improving-processing-performance-parallelforeach
+    // https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/task-parallel-library-tpl
+    // https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/task-parallel-library-tpl
+
+    // System.Threading.Tasks.Parallel, System.Linq.Parallel, Microsoft.TPL.DataFlow
+    // chrome --kiosk "https://www.example.com"
+    public class BoyerMoore
+    {
+
+
+        public static int[] SearchString(string str, string pat)
+        {
+            System.Collections.Generic.List<int> retVal = 
+                new System.Collections.Generic.List<int>();
+
+            int m = pat.Length;
+            int n = str.Length;
+
+            int[] badChar = new int[256];
+
+            BadCharHeuristic(pat, m, ref badChar);
+
+            int s = 0;
+            while (s <= (n - m))
+            {
+                int j = m - 1;
+
+                while (j >= 0 && pat[j] == str[s + j])
+                    --j;
+
+                if (j < 0)
+                {
+                    retVal.Add(s);
+                    s += (s + m < n) ? m - badChar[str[s + m]] : 1;
+                }
+                else
+                {
+                    s += System.Math.Max(1, j - badChar[str[s + j]]);
+                }
+            }
+
+            return retVal.ToArray();
+        }
+
+
+        private static void BadCharHeuristic(string str, int size, ref int[] badChar)
+        {
+            int i;
+
+            for (i = 0; i < 256; i++)
+                badChar[i] = -1;
+
+            for (i = 0; i < size; i++)
+                badChar[(int)str[i]] = i;
+        }
+
+
+    }
+
+
+}
---
+++ WebRansack/Code/SearchAlgorithms/FSGrep.cs
@@ -0,0 +1,90 @@
+﻿
+namespace WebRansack.Code.SearchAlgorithms
+{
+
+
+    public class FSGrep
+    {
+
+
+        public FSGrep()
+        {
+            this.Recursive = true;
+        }
+
+
+        public string RootPath { get; set; }
+        public bool Recursive { get; set; }
+        public string FileSearchMask { get; set; }
+        public string FileSearchLinePattern { get; set; }
+
+
+        public System.Collections.Generic.IEnumerable<string> GetFileNames()
+        {
+            if (!System.IO.Directory.Exists(this.RootPath))
+                throw new System.ArgumentException("GetFileNames() -- Can't find RootPath!");
+
+            if (string.IsNullOrWhiteSpace(this.FileSearchMask))
+                throw new System.ArgumentException("GetFileNames() -- FileSearchPattern is empty; use *.*!");
+
+            System.IO.SearchOption searchOptions = System.IO.SearchOption.AllDirectories;
+            if (!Recursive)
+                searchOptions = System.IO.SearchOption.TopDirectoryOnly;
+
+            if (FileSearchMask.Contains(','))
+            {
+                string[] masks = FileSearchMask.Split(',');
+                System.Collections.Generic.IEnumerable<string> results = 
+                    System.IO.Directory.EnumerateFiles(this.RootPath, masks[0], searchOptions);
+
+                if (masks.Length > 1)
+                {
+                    for (int index = 1; index < masks.Length; index++)
+                    {
+                        results = System.Linq.Enumerable.Concat(results, System.IO.Directory.EnumerateFiles(this.RootPath, masks[index], searchOptions));
+                    }
+                }
+                return results;
+            }
+            else
+            {
+                return System.IO.Directory.EnumerateFiles(this.RootPath, this.FileSearchMask, searchOptions);
+            }
+        }
+
+
+        public System.Collections.Generic.IEnumerable<Result> GetMatchingFiles()
+        {
+            foreach (string filePath in GetFileNames())
+            {
+                int lineNumber = 0;
+                foreach (string line in System.IO.File.ReadAllLines(filePath))
+                {
+                    if (System.Text.RegularExpressions.Regex.Match(line, this.FileSearchLinePattern).Success)
+                        yield return new Result() { FilePath = filePath, FileName = System.IO.Path.GetFileName(filePath), LineNumber = lineNumber, Line = line };
+
+                    lineNumber++;
+                }
+            }
+        }
+
+
+        public class Result
+        {
+            public string FilePath { get; set; }
+            public string FileName { get; set; }
+            public string Line { get; set; }
+            public int LineNumber { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("--file {0}:{1}", FilePath, LineNumber);
+            }
+
+        }
+
+
+    }
+
+
+}
